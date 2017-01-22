9712c93bd1f644c649c3920e0d94a7803796f8f0
Aryan <aryan.gomes@gmail.com>
2017-01-21 22:00:38 -0300

    Implementado conversão de binário para decimal

--- js/conversao.js
+++ js/conversao.js
@@ -1,20 +1,53 @@
 $(document).ready(function(){
-    
-    $("#decimal").keyup(function () {
 
-    	var decimal = $(this).val();
+	$("#decimal").keyup(function () {
 
-    	 $("#binario").val((toBinario(decimal)));
-    });
+		var decimal = $(this).val();
+
+		$("#binario").val((toBinario(decimal)));
+
+	});
 
 
-     $("#decimal").change(function () {
+	$("#decimal").change(function () {
 
-    	var decimal = $(this).val();
+		var decimal = $(this).val();
 
-    	 $("#binario").val((toBinario(decimal)));
-    });
-   
+		$("#binario").val((toBinario(decimal)));
+
+	});
+
+	$("#binario").keyup(function () {
+
+		var binario = $(this).val();
+
+		if(verificaCampo(binario,"[2-9]")){
+			$(this).css('border-color','red');
+			$("[data-toggle='tooltip']").tooltip('show');
+		}else{
+			$(this).css('border-color','blue');
+			$("[data-toggle='tooltip']").tooltip('hide');
+			$("#decimal").val((binarioToDecimal(binario)));
+		}
+
+	});
+
+
+	$("#binario").change(function () {
+
+		var binario = $(this).val();
+
+		if(verificaCampo(binario,"[2-9]")){
+			$(this).css('border-color','red');
+			$("[data-toggle='tooltip']").tooltip('show');
+		}else{
+			$(this).css('border-color','blue');
+			$("[data-toggle='tooltip']").tooltip('hide');
+			$("#decimal").val((binarioToDecimal(binario)));
+		}
+
+	});
+
 });
 
 //Converte um número decimal para binário
@@ -33,7 +66,7 @@ function toBinario(decimal) {
 		binario += ""+(novoValor % 2);
 
 		novoValor = parseInt(novoValor / 2);
-		 
+
 	}
 
 	return reverse(binario);
@@ -42,6 +75,29 @@ function toBinario(decimal) {
 //Reverte os caracteres de um texto
 function reverse(text) {
 
- return text.split("").reverse().join("");
- 
+	return text.split("").reverse().join("");
+
+}
+
+//Converte um número binário para decimal
+function binarioToDecimal(binario) {
+	
+	var arrayBinario = reverse(binario);
+
+	var decimal = 0;
+
+	for (var i = 0; i < arrayBinario.length; i++) {
+		
+		if (i == 0 && (parseInt(arrayBinario[i]) == 1)) {
+			decimal =1;
+		}else if((parseInt(arrayBinario[i]) == 1)){
+			decimal += Math.pow(2,i) ;
+		}
+	}
+
+	return decimal;
+}
+
+function verificaCampo(text, regex) {
+	return new RegExp(regex).test(text);
 }
